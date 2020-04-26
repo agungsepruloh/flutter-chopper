0c0b4b6bd87cb132b1456dc51c40cfd54f8febca
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-04-26 10:01:08 +0700

    Converters and built value integration

--- lib/data/post_api_service.dart
+++ lib/data/post_api_service.dart
@@ -1,5 +1,8 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:chopper/chopper.dart';
 
+import '../model/built_post.dart';
+import 'built_value_converter.dart';
 import 'mobile_data_interceptor.dart';
 
 part 'post_api_service.chopper.dart';
@@ -7,37 +10,38 @@ part 'post_api_service.chopper.dart';
 @ChopperApi(baseUrl: '/posts')
 abstract class PostApiService extends ChopperService {
   @Get()
-  Future<Response> getPosts();
+  Future<Response<BuiltList<BuiltPost>>> getPosts();
 
   @Get(path: '/{id}')
-  Future<Response> getPost(@Path('id') int id);
+  Future<Response<BuiltPost>> getPost(@Path('id') int id);
 
   @Post()
-  Future<Response> postPost(
-    @Body() Map<String, dynamic> body,
+  Future<Response<BuiltPost>> postPost(
+    @Body() BuiltPost body,
   );
 
   static PostApiService create() {
     final client = ChopperClient(
       baseUrl: 'https://jsonplaceholder.typicode.com',
       services: [_$PostApiService()],
-      converter: JsonConverter(),
+      converter: BuiltValueConverter(),
       interceptors: [
+        HttpLoggingInterceptor(), // logging the http interceptor
+        MobileDataInterceptor(), // Custom interceptor
+
         // HeadersInterceptor({'Cache-Control': 'no-cache'}),  // Headers interceptor
-        // HttpLoggingInterceptor(), // logging the http interceptor
         // CurlInterceptor(), // Curl interceptor
-        (Request request) async {
-          if (request.method == HttpMethod.Post)
-            chopperLogger.info('Performed a POST request');
-          return request; // don't forget to return the request
-        },
-        (Response response) async {
-          if (response.statusCode == 404) {
-            chopperLogger.severe('404 Not Found');
-          }
-          return response;
-        },
-        MobileDataInterceptor(),
+        // (Request request) async {
+        //   if (request.method == HttpMethod.Post)
+        //     chopperLogger.info('Performed a POST request');
+        //   return request; // don't forget to return the request
+        // },
+        // (Response response) async {
+        //   if (response.statusCode == 404) {
+        //     chopperLogger.severe('404 Not Found');
+        //   }
+        //   return response;
+        // },
       ],
     );
 
---
+++ lib/model/built_post.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+
+import 'serializers.dart';
+
+part 'built_post.g.dart';
+
+abstract class BuiltPost implements Built<BuiltPost, BuiltPostBuilder> {
+  // fields go here
+  @nullable
+  int get id;
+
+  String get title;
+  String get body;
+
+  BuiltPost._();
+
+  factory BuiltPost([updates(BuiltPostBuilder b)]) = _$BuiltPost;
+
+  String toJson() {
+    return json.encode(serializers.serializeWith(BuiltPost.serializer, this));
+  }
+
+  static BuiltPost fromJson(String jsonString) {
+    return serializers.deserializeWith(
+        BuiltPost.serializer, json.decode(jsonString));
+  }
+
+  static Serializer<BuiltPost> get serializer => _$builtPostSerializer;
+}
