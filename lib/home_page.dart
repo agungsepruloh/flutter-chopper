6bb4682888838aa924b78e383bab5b51604161d0
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-04-26 10:01:45 +0700

    Implement converters and built value on the UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,10 @@
-import 'dart:convert';
-
+import 'package:built_collection/built_collection.dart';
 import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'data/post_api_service.dart';
+import 'model/built_post.dart';
 import 'single_post_page.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,9 +18,21 @@ class HomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
-          final response =
-              await Provider.of<PostApiService>(context, listen: false)
-                  .postPost({'key': 'value'});
+          // Use BuiltPost even for POST requests
+          final newPost = BuiltPost(
+            (b) => b
+              // id is null - it gets assigned in the backend
+              ..title = 'New Title'
+              ..body = 'New body',
+          );
+
+          // The JSONPlaceholder API always responds with whatever was passed in the POST request
+          final response = await Provider.of<PostApiService>(
+            context,
+            listen: false,
+          ).postPost(newPost);
+          // We cannot really add any new posts using the placeholder API,
+          // so just print the response to the console
           print(response.body);
         },
       ),
@@ -28,23 +40,26 @@ class HomePage extends StatelessWidget {
   }
 
   FutureBuilder<Response> _buildBody(BuildContext context) {
-    return FutureBuilder<Response>(
+    // Specify the type held by the Response
+    return FutureBuilder<Response<BuiltList<BuiltPost>>>(
       future: Provider.of<PostApiService>(context).getPosts(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
+          // Exceptions thrown by the Future are stored inside the "error" field of the AsyncSnapshot
           if (snapshot.hasError) {
             return Center(
               child: Text(
                 snapshot.error.toString(),
                 textAlign: TextAlign.center,
-                textScaleFactor: 1.5,
+                textScaleFactor: 1.3,
               ),
             );
           }
-
-          final List posts = json.decode(snapshot.data.bodyString);
+          //* Body of the response is now type-safe and of type BuiltList<BuiltPost>.
+          final posts = snapshot.data.body;
           return _buildPosts(context, posts);
         } else {
+          // Show a loading indicator while waiting for the posts
           return Center(
             child: CircularProgressIndicator(),
           );
@@ -53,7 +68,8 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  ListView _buildPosts(BuildContext context, List posts) {
+  // Changed the parameter type.
+  ListView _buildPosts(BuildContext context, BuiltList<BuiltPost> posts) {
     return ListView.builder(
       itemCount: posts.length,
       padding: EdgeInsets.all(8),
@@ -62,11 +78,11 @@ class HomePage extends StatelessWidget {
           elevation: 4,
           child: ListTile(
             title: Text(
-              posts[index]['title'],
+              posts[index].title,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            subtitle: Text(posts[index]['body']),
-            onTap: () => _navigateToPost(context, posts[index]['id']),
+            subtitle: Text(posts[index].body),
+            onTap: () => _navigateToPost(context, posts[index].id),
           ),
         );
       },
--- lib/single_post_page.dart
+++ lib/single_post_page.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'data/post_api_service.dart';
+import 'model/built_post.dart';
 
 class SinglePostPage extends StatelessWidget {
   final int postId;
@@ -16,11 +15,11 @@ class SinglePostPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Chopper Blog'),
       ),
-      body: FutureBuilder<Response>(
+      body: FutureBuilder<Response<BuiltPost>>(
         future: Provider.of<PostApiService>(context).getPost(postId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            final Map post = json.decode(snapshot.data.bodyString);
+            final post = snapshot.data.body;
             return _buildPost(post);
           } else {
             return Center(
@@ -32,17 +31,17 @@ class SinglePostPage extends StatelessWidget {
     );
   }
 
-  Padding _buildPost(Map post) {
+  Padding _buildPost(BuiltPost post) {
     return Padding(
       padding: EdgeInsets.all(8),
       child: Column(
         children: <Widget>[
           Text(
-            post['title'],
+            post.title,
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
-          Text(post['body']),
+          Text(post.body),
         ],
       ),
     );
